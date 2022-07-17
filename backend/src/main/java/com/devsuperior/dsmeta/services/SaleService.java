9533f367ba5faa70f3320b348f9ddad2d693fb5b
wilrrama <wilrrama@hotmail.com>
2022-07-17 19:41:49 -0300

    Toast

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -22,7 +22,7 @@ public class SaleService {
 	public Page<Sale>findSales(String minDate, String maxDate, Pageable pageable) {
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 		
-		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
 		return repository.findSales(min, max,pageable);
